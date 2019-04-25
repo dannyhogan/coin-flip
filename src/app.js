389d1a7be2db0f900f1594d71c826cb036e6a30b
Dan Hogan <hogan.daniel@hotmail.com>
2019-04-24 19:40:32 -0700

    Added user guess to variable based off radio button click

--- src/app.js
+++ src/app.js
@@ -5,11 +5,17 @@ const lossCounter = document.getElementById('loss-counter');
 const coinImage = document.getElementById('coin-image');
 const displayMessage = document.getElementById('display-message');
 const headsRadio = document.getElementById('heads-radio');
-const tailsRadio = document.getElementById('tails-radio');
 const flipButton = document.getElementById('flip-button');
 
 flipButton.addEventListener('click', () => {
     const randomNumber = Math.random();
     const result = headsOrTails(randomNumber);
-    console.log(r
+
+    let guess = '';
+    if(headsRadio.checked) {
+        guess = 'heads';
+    } else {
+        guess = 'tails';
+    }
+    console.log(result, guess);
 });
