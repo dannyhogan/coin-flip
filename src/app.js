a7bd495f3b2c745cfb8ae708aa528870e70b8d3e
dannyhogan <hogan.daniel@hotmail.com>
2019-04-24 17:51:02 -0700

    Add files via upload
    Added initial files for coin flip, set up basic HTML, and linked elements to app.js

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const winCounter = document.getElementById('win-counter');
+const lossCounter = document.getElementById('loss-counter');
+const coinImage = document.getElementById('coin-image');
+const displayMessage = document.getElementById('display-message');
+const headsRadio = document.getElementById('heads-radio');
+const tailsRadio = document.getElementById('tails-radio');
+const flipButton = document.getElementById('flip-button');
+
+flipButton.addEventListener('click', () => {
+    console.log('hello world');
+});
