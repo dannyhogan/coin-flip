184e2aab61c91bfa3e689644abed373c4a2465fc
Dan Hogan <hogan.daniel@hotmail.com>
2019-04-24 18:24:54 -0700

    Added two test cases and wrote function to determine heads or tails

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,30 @@
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
+function headsOrTails(number) {
+    if(number < .5) {
+        return 'heads';
+    }
+    return 'tails'
+}
 
+test('return a value of heads if the randomNumber is below .5', function(assert) {
+    //Arrange
+    const number = .3;
+    const expected ='heads'
     //Act 
-    // Call the function you're testing and set the result to a const
+    const result = headsOrTails(number);
 
     //Assert
-    assert.equal(true, false);
+    assert.equal(result, expected);
+});
+
+test('return a value of tails if the randomNumber is above .5', function(assert) {
+    //Arrange
+    const number = .6;
+    const expected ='tails'
+    //Act 
+    const result = headsOrTails(number);
+
+    //Assert
+    assert.equal(result, expected);
 });
